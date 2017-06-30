eff7a0b402e6b80e2467dd6ad0fb04cd31168636
Ole Ersoy <ole.ersoy@gmail.com>
2017-06-30 00:01:39 -0500

    Updated project to support deployment

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,4 @@ require('@superflycss/task-clean');
 require('@superflycss/task-build');
 require('@superflycss/task-test');
 require('@superflycss/task-lint');
-
-
-var PLI = require('@superflycss/pli');
-
-var gulp = require('gulp');
-
-gulp.task('watch', function() {
-  gulp.watch(PLI.SRC_MAIN_CSS, ['build:css']);
-  gulp.watch(PLI.SRC_TEST_CSS, ['build:css']);
-  gulp.watch(PLI.SRC_TEST_HTML, ['test:css']);
-});
+require('@superflycss/task-deploy');
