e84f9079bf9ac6a73cf04f22dc39d5736a16c42e
Ole <ole.ersoy@gmail.com>
2018-05-13 22:10:18 -0500

    Removed gulp dependencies

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-require('@superflycss/task-clean');
-require('@superflycss/task-build');
-require('@superflycss/task-test');
-require('@superflycss/task-lint');
-require('@superflycss/task-deploy');
